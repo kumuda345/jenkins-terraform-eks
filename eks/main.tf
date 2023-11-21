98a878ae8a00e6a2610683b11990b6313da9c0e0
kumuda345 <100067793+kumuda345@users.noreply.github.com>
2023-11-21 17:37:47 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -39,6 +39,8 @@ module "eks" {
   cluster_name    = "terraform-eks"
   cluster_version = "1.27"
 
+  cluster_endpoint_public_access = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
